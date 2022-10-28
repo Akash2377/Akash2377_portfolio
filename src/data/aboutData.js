c24ae6d2a240646be040d4b973b965e8e91167f1
Akash Surve <surveakash01@gmail.com>
2022-10-28 15:26:59 +0530

    grammer check

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,8 +1,8 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "Hello stranger! 👋, my name is Akash Surve and I am a Full Stack Developer, passionate about building digital products that improve everyday experience for people.",
+    "Hello stranger! 👋, my name is Akash Surve and I am a Full Stack Developer, passionate about building digital products that improve the everyday experience for people.",
   description2:
-    "I am Quick learner and an aspiring full-stack web developer with core knowledge of MERN stack technology. Looking forward to applying and enhancing my skills and knowledge as a developer.",
+    "I am a Quick learner and an aspiring full-stack web developer with core knowledge of MERN stack technology. Looking forward to applying and enhancing my skills and knowledge as a developer.",
   image: 1,
 };
